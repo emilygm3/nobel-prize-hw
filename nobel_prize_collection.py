d0a42c63e96396cd5b5d72011a3c7bdb20ac17d4
eleanorlopez <lopez.ele@northeastern.edu>
2026-02-23 20:52:57 -0500

    Add files via upload

--- nobel_prize_collection.py
+++ nobel_prize_collection.py
@@ -10,6 +10,7 @@ with open('laureate.json') as json_file:
     data = json.load(json_file)
 
 prize_data = data["laureates"]
+db.collection.delete_many({})
 result = db.collection.insert_many(prize_data)
 
 
@@ -17,3 +18,25 @@ result = db.collection.insert_many(prize_data)
 #
 # for prize in chemistry_prizes:
 #     pprint.pprint(prize)
+
+# countries = db.collection.distinct("bornCountry")
+# print(countries)
+
+
+pipeline = [
+    {"$match": {"bornCountry": {"$exists": True}}},
+    {"$group": {
+        "_id": "$bornCountry",
+        "count": {"$sum": 1}
+    }},
+    {"$sort": {"count": -1}},
+    {"$limit": 10}
+]
+
+results = db.collection.aggregate(pipeline)
+
+for doc in results:
+    print(doc["_id"], doc["count"])
+
+
+
